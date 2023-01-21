4a54b3e828cf5dc5898f4214041d591dd9109fa5
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-21 02:20:17 -0600

    Updated Clock.js -Now generates an SVG with rotating circles

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -1,21 +1,64 @@
 import React from "react";
-import "./Clock.module.css";
+import css from "./Clock.module.css";
 /**
  * React Component for tracking time, displayed as a <p> element.
 */
 class Clock extends React.Component{
     constructor(props){
         super(props);
-        this.state = { time: new Date() };
+        this.state = {
+            time: new Date(),
+            levels: [
+                {
+                    getValue: function(date){ return date.getSeconds(); },
+                    increment: 60,
+                    color: "green",
+                },
+                {
+                    getValue: function(date){ return date.getMinutes(); },
+                    increment: 60,
+                    color: "blue",
+                },
+                {
+                    getValue: function(date){ return date.getHours(); },
+                    increment: 24,
+                    color: "red",
+                }
+            ],
+            radius: 10,
+            strokeSize: 2,
+            gap: 10
+        };
     }
     componentDidMount(){
         this.timerID = setInterval(() => this.tick(), 1000);
     }
-    tick = () =>{ 
-        this.setState({ time: new Date() }); 
+    componentWillUnmount(){
+        clearInterval(this.timerID);
     }
+    getRadius = () => this.state.radius+this.state.gap+this.state.strokeSize;
+    tick = () => this.setState({ time: new Date() });
+    clockStroke = (tick, radius, increment) => {
+        let circ = 2*Math.PI*radius;
+        let inc = circ/increment;
+        let fill = (tick+1)*inc;
+        return fill+" "+(circ-fill);
+    };
     render(){ 
-        return( <p className="clock-component">{this.state.time.toLocaleTimeString()}</p> ); 
+        return(
+            <svg className={css.clock_component}>
+                {this.state.levels.map((level, index) => 
+                    <circle
+                        key={ index }
+                        className={ css.clock_circle }
+                        r={ this.getRadius().toString()+"px" }
+                        color={ level.color }
+                        strokeWidth={ this.state.strokeSize }
+                        strokeDasharray={ this.clockStroke(level.getValue(this.state.time),this.getRadius(), level.increment) }
+                    />
+                )}
+            </svg>
+        );
     }
 }
 
