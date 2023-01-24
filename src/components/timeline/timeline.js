8f8018f1b9eb6a32654997035bf643d6f5775acb
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-23 21:32:43 -0600

    updated timeline logic

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -5,21 +5,25 @@ import css from "./timeline.module.css";
 class Timeline extends React.Component {
     constructor(props){ 
         super(props);
-        this.state = { sections: this.props.sections ? this.props.sections : [] };
+        this.state = { 
+            sections: this.props.sections ? this.props.sections : [],
+            active: 0
+        };
     }
     render(){
+        let first = this.state.active == 0 ? 0 :
+            this.state.active == this.state.sections.length-1 ? this.state.sections.length-3 : 
+            this.state.active-1;
+        let last = this.state.active == 0 ? 2 :
+            this.state.active == this.state.sections.length-1 ? this.state.sections.length-1 :
+            this.state.active+1;
+        console.log([first, last]);
         return(
-            <div className={this.props.className}>
-                {this.state.sections.map(section => {
-                    return(
-                        <div>
-                            <p> {section} </p>
-                        </div>
-                    );
-                })}
+            <div id={this.props.id} className={this.props.className+"  "+css.timeline_container}>
+                { this.state.sections.slice(first, last).map((section, index) => <p style={{color: "white"}} key={index}> {section} </p>) }
             </div>
         );
     }
 }
-
+//
 export default Timeline;
