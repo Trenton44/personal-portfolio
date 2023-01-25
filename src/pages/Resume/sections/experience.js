c6056fd03d47f40de84295be946f17add5ba71ca
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-24 22:32:15 -0600

    Converted Experience points into <ul> list. -Added uri fragment id to headers

--- src/pages/Resume/sections/experience.js
+++ src/pages/Resume/sections/experience.js
@@ -7,18 +7,17 @@ class Experience extends React.Component {
     render(){
         return(
             <div className={css.main_box}>
-                <h1> Experience </h1>
+                <h1 id="#Experience"> Experience </h1>
                 <div className={css.job_content}>
                     <div className={css.job_header}>
                         <h1> Associate Application Developer - IBM </h1>
                         <h1> March 2022 - Present</h1>
                     </div>
                     <h3 style={{ margin: "5px" }}>Worked on Agile projects involving</h3>
-                    <p>
-                    Lift & Shifting client databases to AWS Cloud to lower cost and reduce processing times
-                    <br />
-                    {"Refactoring batch scripts (C++) to reduces processing times from 4 hours to <1"}
-                    </p>
+                    <ul>
+                        <li> Lift & Shifting client databases to AWS Cloud to lower cost and reduce processing times </li>
+                        <li> {"Refactoring batch scripts (C++) to reduces processing times from 4 hours to <1"} </li>
+                    </ul>
                 </div>
                 <div className={css.job_content}>
                     <div className={css.job_header}>
@@ -26,25 +25,23 @@ class Experience extends React.Component {
                         <h1> March 2021 - March 2022 </h1>
                     </div>
                     <h3 style={{ margin: "5px" }}> Studied and trained under mentors and senior developers through the IBM Apprentice Program </h3>
-                    <p>
-                    Developed and tested projects created by myself and other apprentices in multiple languages
-                    <br />
-                    Expanded my knowledge in multiple languages, such as C/C++, NodeJS, Python, ReactJS, HTML/CSS, and Java
-                    <br />
-                    Gained experience with several notable technologies: AWS & IBM Cloud, GitHub, Kubernetes/Docker, Linux, and CI/CD pipelines such as AWS CodePipeline and Jenkins
-                    </p>
+                    <ul>
+                        <li> Developed and tested projects created by myself and other apprentices in multiple languages </li>
+                        <li> Expanded my knowledge in multiple languages, such as C/C++, NodeJS, Python, ReactJS, HTML/CSS, and Java </li>
+                        <li> Gained experience with several notable technologies: AWS & IBM Cloud, GitHub, Kubernetes/Docker, Linux, and CI/CD pipelines such as AWS CodePipeline and Jenkins </li>
+                    </ul>
                 </div>
                 <div className={css.job_content}>
                     <div className={css.job_header}>
                         <h1> IT Technician Intern - Bossier Parish School Board </h1>
                         <h1> March 2019 - March 2021 </h1>
                     </div>
+                    
                     <h3 style={{ margin: "5px" }}> Accompanied senior technicians to parish schools & centers, resolving support tickets </h3>
-                    <p>
-                    Setup & repair technology for district employees
-                    <br />
-                    Install & maintain network infrastructure
-                    </p>
+                    <ul>
+                        <li> Setup & repair technology for district employees </li>
+                        <li> Install & maintain network infrastructure </li>
+                    </ul>
                 </div>
                 <div className={css.job_content}>
                     <div className={css.job_header}>
@@ -52,13 +49,11 @@ class Experience extends React.Component {
                         <h1> June 2020 - November 2020 </h1>
                     </div>
                     <h3 style={{ margin: "5px" }}> Coordinator and Senior lead of the audio/visual teams for Sunday Services </h3>
-                    <p>
-                    Lead video/audio teams during Sunday Services
-                    <br />
-                    Install and maintain audio/video equipment and verify functionality for upcoming services
-                    <br />
-                    Coordinate & train volunteers
-                    </p>
+                    <ul>
+                        <li> Lead video/audio teams during Sunday Services </li>
+                        <li> Install and maintain audio/video equipment and verify functionality for upcoming services </li>
+                        <li> Coordinate & train volunteers </li>
+                    </ul>
                 </div>
             </div>
         );
--- src/pages/Resume/sections/summary.js
+++ src/pages/Resume/sections/summary.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import css from "./summary.module.css";
-import MyPicture from "../../../images/PlaceholderPerson.jpg";
+import MyPicture from "../../../images/Me.jpg";
 
 class Summary extends React.Component{
     constructor(props){
@@ -11,7 +11,7 @@ class Summary extends React.Component{
         return(
             <div className={css.body}>
                 <div className={css.left_content}>
-                    <h1> Summary </h1>
+                    <h1 id="#Summary"> Summary </h1>
                     <p> Trenton is a software developer with skills in web development, particularly in the ReactJS framework, &
                     NodeJS. <br /> He has work experience with cloud computing, and is a certified AWS Developer Associate. Coming
                     from a background in Object-Oriented Programming, as a graduate from Bossier Parish Community College &
@@ -25,7 +25,7 @@ class Summary extends React.Component{
                     <div></div>
                 </div>
                 <div className={css.right_content}>
-                    <img id="my-portrait" src={MyPicture}/>
+                    <img className={css.my_picture} src={MyPicture}/>
                 </div>
             </div>
         );
