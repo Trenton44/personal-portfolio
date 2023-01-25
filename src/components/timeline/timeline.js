35b3e420401a49389a868b00c06340903addcb27
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-24 22:30:31 -0600

    timeline now correctly jumps to elements with id matching the uri frag

--- src/components/timeline/timeline.js
+++ src/components/timeline/timeline.js
@@ -9,10 +9,14 @@ class Timeline extends React.Component {
             sections: this.props.sections ? this.props.sections : []
         };
     }
+    changeLocation = (urifrag) => {
+        window.location.hash = urifrag;
+        document.getElementById(window.location.hash).scrollIntoView();
+    };
     render(){
         return(
             <div id={this.props.id} className={this.props.className+"  "+css.timeline_container}>
-                { this.state.sections.map((section, index) => <p className={css.timeline_item} key={index}> {section} </p>) }
+                { this.state.sections.map((section, index) => <p onClick={() => this.changeLocation(section)} className={css.timeline_item} key={index}> {section} </p>) }
             </div>
         );
     }
