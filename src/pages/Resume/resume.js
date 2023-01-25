68a4451acbb87d40da1fd43c4363e5f3e5d5f5f6
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-24 22:31:17 -0600

    Updated the resume page -added background gradient -Removed two sections -Added Education section to page

--- src/pages/Resume/resume.js
+++ src/pages/Resume/resume.js
@@ -5,22 +5,27 @@ import css from "./resume.module.css";
 import Timeline from "../../components/timeline/timeline.js";
 import Summary from "./sections/summary.js";
 import Experience from "./sections/experience.js";
+import Education from "./sections/education";
 
 class Resume extends React.Component{
     constructor(props){
         super(props);
         this.state = {
             active: 0,
-            sections: ["Summary", "Experience", "Education", "Certifications", "Skills"]
+            sections: ["Summary", "Experience", "Education"]
          };
     }
     componentDidMount(){
         let resume = document.getElementById("resume-content");
         let timeline = document.getElementById("resume-timeline");
-        console.log(resume);
-        console.log(timeline);
-        resume.addEventListener("scroll", (e) => timeline.scrollTop = resume.scrollTop);
-        timeline.addEventListener("scroll", (e) => resume.scrollTop = timeline.scrollTop);
+        console.log(resume.scrollTop);
+        console.log(timeline.scrollTop);
+        resume.addEventListener("scroll", (e) => {
+            console.log(resume.scrollTop);
+            console.log(timeline.scrollTop);
+            timeline.scrollTop = resume.scrollTop;
+        });
+        //timeline.addEventListener("scroll", (e) => resume.scrollTop = timeline.scrollTop);
     }
     render(){
         return(
@@ -34,6 +39,7 @@ class Resume extends React.Component{
                         <Experience />
                     </div>
                     <div className={css.content_box}>
+                        <Education />
                     </div>
                     <div className={css.content_box}>
                     </div>
