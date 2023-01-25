22a7e43b9196f911b9aaad30f3992c561b86fdd9
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-24 21:29:49 -0600

    Removed about.js, add initial code for projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,10 +1,20 @@
 import React from "react";
 import css from "./projects.module.css";
+import { default as ImageContainer } from "../components/sliced_image_content/container.js";
 
 class Projects extends React.Component {
     constructor(props){ super(props); }
     render(){
-        return(<div></div>);
+        return(
+        <div className={css.main_content}>
+            <div className={css.project_container}>
+                <ImageContainer>
+                    <h1> Hello World! </h1>
+                    <h2> How are you? </h2>
+                </ImageContainer>
+            </div>
+        </div>
+        );
     }
 }
 
