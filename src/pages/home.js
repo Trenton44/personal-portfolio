ca376d042128b229e208165f6ea4c18551d7d7d7
Trenton44 <38508613+Trenton44@users.noreply.github.com>
2023-01-31 17:56:22 -0600

    Refactoring home.js to new components

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,25 +1,26 @@
 import React from "react";
-import css from "./home.module.css";
-import Me from "../images/Me.jpg";
 
+import "./pages.css";
+import css from "./home.module.css";
+
+import DImgC from "../components/diagonal_img_content/d_img_c.js";
+import Footer from "../components/PageFooter/footer.js";
+
+import Me from "../images/Me.jpg";
 import LinkedInImage from "../images/LinkedIn.png";
 const GmailLogo = "https://upload.wikimedia.org/wikipedia/commons/7/7e/Gmail_icon_%282020%29.svg";
 
 class Home extends React.Component {
-    constructor(props){ super(props); }
+    constructor(props) { super(props); }
     render(){
         return(
-        <div className={css.content_box}>
-            <div className={css.title_content}> 
-                <h1> Trenton Choate </h1>
-                <h2> Fullstack Developer </h2>
-            </div>
-            <div className={css.about_content}>
-                <div className={css.about_box_cutter}>
-                    <img className={css.self_picture} src={Me} />
+            <div className={"content_container "+css.home_container}>
+                <div className={css.title_content}>
+                    <h1> Trenton Choate </h1>
+                    <h2> Fullstack Developer </h2>
                 </div>
-                <div className={css.right_content_container}>
-                    <div className={css.right_content_box}>
+                <DImgC src={Me}className={css.about_container}>
+                    <div className={css.DimgC_content}>
                         <h1> About Me </h1>
                         <h2> Associate Application Developer @IBM </h2>
                         <h3> Graduated Bossier Parish Community College </h3>
@@ -32,29 +33,26 @@ class Home extends React.Component {
                         </p>
                         <a href="https://www.credly.com/users/trenton-choate"> View Credentials </a>
                     </div>
-                </div>
+                </DImgC>
+                <div style={{height: "400px", width: "100%"}}></div>
+                <Footer items={
+                    [
+                        {
+                            href:"https://www.linkedin.com/in/trenton44/",
+                            src: LinkedInImage,
+                            description: "LinkedIn"
+                        },
+                        {
+                            href: "mailto:tchoate44@gmail.com",
+                            src: GmailLogo,
+                            description: "tchoate44@gmail.com"
+                        }
+                    ]
+                }/>
             </div>
-            <div style={{height: "400px", width: "100%"}}></div>
-            <footer className={css.page_footer}>
-                <h1> Contact: </h1>
-                <div className={css.socials_content}>
-                    <a href="https://www.linkedin.com/in/trenton44/">
-                        <img src={LinkedInImage} />
-                        <p> LinkedIn </p>
-                    </a>
-                    <a href="mailto:tchoate44@gmail.com">
-                        <img src={GmailLogo} />
-                        <p>tchoate44@gmail.com</p>
-                    </a>
-                </div>
-                <div className={css.background_credit}>
-                    <a href="https://commons.wikimedia.org/wiki/File:Usa-world-trade-center-skyscrapers-reflection-night-skyline-cityscape.jpg">Background Image Credit: Kozak4512</a>
-                    <a href="https://creativecommons.org/licenses/by-sa/4.0">CC BY-SA 4.0</a>
-                </div>
-                
-            </footer>
-        </div>
+            
         );
     }
 }
+
 export default Home;
